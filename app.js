afeadfcb78a19fd549f60a019bce27e00105b259
devmoi <moishadrack.com>
2022-12-24 13:50:29 +0300

    add variables

---
+++ app.js
@@ -0,0 +1,14 @@
+// Dom Elements
+
+const cityName = document.querySelector(".city");
+const weatherCondition = document.querySelector(".weatherCondition");
+const temp = document.querySelector(".temp");
+const feelsLike = document.querySelector(".feelsLike");
+const max = document.querySelector(".tempMax");
+const humidity = document.querySelector(".humidty")
+const deg = document.querySelector(".deg");
+const input = document.querySelector(".searchBar");
+const submit = document.querySelector(".add");
+const slider = document.querySelector(".toggleF")
+const img = document.querySelector(".weatherImage")
+const body = document.querySelector("body")
